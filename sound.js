f6866151145098fc0ad52563511337f1049e054f
Benjamin Jeffery <bjeffery@kx.com>
2024-05-01 22:33:58 -0400

    Made the sound quieter

--- sound.js
+++ sound.js
@@ -11,8 +11,16 @@ function startBeep() {
 
   // Create an oscillator
   oscillator = audioContext.createOscillator();
-  oscillator.type = "sine"; // You can change this to 'square', 'sawtooth', or 'triangle'
+  oscillator.type = "sine";
   oscillator.frequency.setValueAtTime(440, audioContext.currentTime); // Frequency in Hertz (A4 note)
+
+  // Create a GainNode
+  const gainNode = audioContext.createGain();
+  gainNode.gain.setValueAtTime(0.1, audioContext.currentTime); // Set initial gain (50% volume)
+
+  // Connect the oscillator to the gain node and the gain node to the destination
+  oscillator.connect(gainNode);
+  gainNode.connect(audioContext.destination);
 
   // Connect the oscillator to the audio context's destination
   oscillator.connect(audioContext.destination);
