8b1959e049f453a38b28ee48b7fbf5c5fdf4be61
Benjamin Jeffery <bjeffery@kx.com>
2024-06-09 15:42:07 -0400

    Fixed some issues on mobile. Added TODO and a license

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+// TODO
+// - Practice mode doesn't work after the explosion
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+// AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 const RESPONSE_DELAY = 3_000; // milliseconds
 const TIME_UNTIL_EXPLOSION = 120_000; // milliseconds
 
@@ -148,11 +166,17 @@ document.addEventListener("keyup", function (event) {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  document.body.addEventListener("touchstart", function (event) {
+  document.body.addEventListener("touchstart", function (e) {
     console.log("start");
+    e.preventDefault();
+    e.stopPropagation();
+    press();
   });
-  document.body.addEventListener("touchend", function (event) {
+  document.body.addEventListener("touchend", function (e) {
     console.log("stop");
+    e.preventDefault();
+    e.stopPropagation();
+    release();
   });
 });
 
