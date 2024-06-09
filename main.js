11964fd8c6be76941d7e0119494451324ee07f8b
Benjamin Jeffery <bjeffery@kx.com>
2024-06-09 15:26:08 -0400

    Added a practice mode, and significant refactoring

--- main.js
+++ main.js
@@ -1,32 +1,91 @@
-let PRACTICE_MODE = false;
-const RESPONSE_DELAY = 3_000;
-var lineText = "";
-
-keyEvents = [];
-var upTime, downTime;
-
-var over,
-  listening = false,
-  first = true;
+const RESPONSE_DELAY = 3_000; // milliseconds
+const TIME_UNTIL_EXPLOSION = 120_000; // milliseconds
 
 var state = {
+  lastKeyEvent: null,
+
+  // In listening mode (when an NPC is replying), the user cannot send messages
+  listening: false,
+
+  // The delay between user inputs that triggers a reply
+  startReplyTimeout: null,
+
+  // The timeout used while an NPC is replying
+  replyTimeout: null,
+
+  explosionTimeout: null,
   started: false,
   practice: false,
   stoppedTrain: false,
   exploded: false,
   narration: false,
+
+  // The current state of the user's reply
+  msg: {
+    lineText: "",
+    keyEvents: [],
+  },
+
+  // The current state of what the user has told Rockingham
+  sent: {
+    fire: false,
+    ship: false,
+    stop: false,
+    train: false,
+  },
 };
+
+function resetMsg() {
+  state.msg = {
+    lineText: "",
+    keyEvents: [],
+  };
+  msg.innerText = "";
+}
+
+function togglePractice(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  stopBeep();
+  state.listening = false;
+
+  state.practice = !state.practice;
+  document.getElementById("clearButton").style.visibility = state.practice
+    ? ""
+    : "hidden";
+  document.getElementById("practiceButton").innerText =
+    "Practice Mode " + (state.practice ? "ON" : "OFF");
+
+  if (state.practice) {
+    clearTimeout(state.explosionTimeout);
+    clearTimeout(state.startReplyTimeout);
+    clearTimeout(state.replyTimeout);
+  } else {
+    startExplosionTimeout();
+  }
+}
+
+function clearMsg(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  resetMsg();
+}
 
 function explode() {
   if (state.exploded) return;
+
+  stopBeep();
 
   state.exploded = true;
   msg.innerText = "";
   document.getElementById("explosionAudio").play();
   view.src = "explosion.png";
-  setTimeout(function () {
+  state.replyTimeout = setTimeout(function () {
+    // Show the ruins of Halifax
     view.src = "aftermath.png";
-    setTimeout(function () {
+    state.replyTimeout = setTimeout(function () {
+      // Play Vincent's message
       text =
         "Hold up the train. " +
         "Ammunition ship afire in harbour making for Pier 6 and will explode. " +
@@ -37,37 +96,47 @@ function explode() {
   }, 6000);
 }
 
+function startExplosionTimeout() {
+  state.explosionTimeout = setTimeout(explode, TIME_UNTIL_EXPLOSION);
+}
+
 function press() {
-  if (listening || state.exploded) return;
+  if (state.listening || state.exploded) return;
 
   if (!state.started) {
-    setTimeout(explode, 120_000);
+    startExplosionTimeout();
     state.started = true;
   }
 
   view.src = "down.png";
-  clearTimeout(over);
+  clearTimeout(state.startReplyTimeout);
 
-  downTime = Date.now();
-  keyEvents.push({ down: false, duration: Date.now() - upTime });
+  state.msg.keyEvents.push({
+    down: false,
+    duration: Date.now() - state.lastKeyEvent,
+  });
+  state.lastKeyEvent = Date.now();
 
   startBeep();
 }
 
 function release() {
-  if (listening || state.exploded) return;
+  if (state.listening || state.exploded) return;
 
   view.src = "up.png";
 
-  upTime = Date.now();
-  keyEvents.push({ down: true, duration: Date.now() - downTime });
+  state.msg.keyEvents.push({
+    down: true,
+    duration: Date.now() - state.lastKeyEvent,
+  });
+  state.lastKeyEvent = Date.now();
 
   stopBeep();
-  if (!PRACTICE_MODE) {
-    over = setTimeout(generateReply, RESPONSE_DELAY);
+  if (!state.practice) {
+    state.startReplyTimeout = setTimeout(generateReply, RESPONSE_DELAY);
   }
 
-  msg.innerText = translate(keyEvents.slice(1));
+  msg.innerText = translate(state.msg.keyEvents.slice(1));
 }
 
 document.addEventListener("keydown", function (event) {
@@ -92,14 +161,8 @@ function getRandomElement(arr) {
   return arr[randomIndex];
 }
 
-sent = {
-  fire: false,
-  ship: false,
-  stop: false,
-  train: false,
-};
-
 function generateReply() {
+  var sent = state.sent;
   normalized = msg.innerText.replace(/ /g, "");
   sent.fire =
     sent.fire || /FIRE|SMOKE|EXPLOSION|FLAME|BLAZE|DANGER/.test(normalized);
@@ -144,22 +207,21 @@ function proseToMorse(text) {
 }
 
 function playMsg(callsign, text) {
-  listening = true;
-  lineText = "";
-  keyEvents = [];
-
+  state.listening = true;
+  resetMsg();
+  msg.innerText = callsign + ": ";
   playMsgCallback(callsign, proseToMorse(text), 0, false);
 }
 
 function playMsgCallback(callsign, code, i, shortPause) {
-  char = code[i];
-  nextChar = code[i + 1];
+  var char = code[i];
+  var nextChar = code[i + 1];
 
   stopBeep();
 
   if (i >= code.length) {
-    listening = false;
-    msg.innerText = toEnglish(callsign, lineText + " ");
+    state.listening = false;
+    msg.innerText = toEnglish(callsign, state.msg.lineText + " ");
 
     if (state.stoppedTrain && !state.exploded) {
       explode();
@@ -172,22 +234,22 @@ function playMsgCallback(callsign, code, i, shortPause) {
   jitter = Math.random(10) - 5;
 
   if (shortPause) {
-    setTimeout(
+    state.replyTimeout = setTimeout(
       playMsgCallback.bind(this, callsign, code, i, false),
       timing["⋅"] + jitter
     );
   } else if (char === "⋅" || char === "-") {
-    lineText += char;
+    state.msg.lineText += char;
     startBeep();
     shortPause = nextChar === "⋅" || nextChar === "-";
-    setTimeout(
+    state.replyTimeout = setTimeout(
       playMsgCallback.bind(this, callsign, code, i + 1, shortPause),
       timing[char] + jitter
     );
   } else {
-    lineText += char;
-    msg.innerText = toEnglish(callsign, lineText);
-    setTimeout(
+    state.msg.lineText += char;
+    msg.innerText = toEnglish(callsign, state.msg.lineText);
+    state.replyTimeout = setTimeout(
       playMsgCallback.bind(this, callsign, code, i + 1, false),
       timing[char] + jitter
     );
